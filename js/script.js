d38f3993ed8bbfe12a6770920ce7dceefe31d784
조성환 <2strike3balls@gmail.com>
2025-01-16 22:20:35 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,34 @@
-$(document).ready(function(){
-    $(".line>li").click(function(){
-        $(this).next('.board-wrap').fadeIn();
-        $('.inner .cross').slideDown();
+$(function () {
+    $('#btn1').click(function () {
+        var height = parseInt($('#height').val()) / 100;
+        var weight = parseInt($('#weight').val());
+        var bmi = 0;
+        var str = '---';
+
+        $('ul').removeClass();
+        bmi = weight / (height * height);
+        if (bmi >= 35) {
+            str = '고도비만';
+            $('ul').addClass('red');
+        } else if (bmi >= 30) {
+            str = '비만';
+            $('ul').addClass('orange');
+        } else if (bmi >= 25) {
+            str = '과체중';
+            $('ul').addClass('yellow');
+        } else if (bmi >= 18) {
+            str = '보통';
+            $('ul').addClass('green');
+        } else if (bmi <= 17) {
+            str = '저체중';
+            $('ul').addClass('aquamarine');
+        } else {
+            bmi = '---'
+            alert('다시 입력해주세요.');
+        }
+        $('.bmi span').text(bmi);
+        $('.status span').text(str);
     });
-    $(".inner .cross").click(function(){
-        $(".board-wrap").fadeOut();
-        $(this).slideUp();
-        });
+
+
 });
