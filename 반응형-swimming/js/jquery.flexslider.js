52199811f69ac058f7f3d1ecb448c9e387282b3c
조성환 <2strike3balls@gmail.com>
2025-02-05 21:47:23 +0900

    02050947

--- 반응형-swimming/js/jquery.flexslider.js
+++ 반응형-swimming/js/jquery.flexslider.js
@@ -346,7 +346,7 @@ $(function () {
       },
       directionNav: {
         setup: function() {
-          var directionNavScaffold = $('<ul class="' + namespace + 'direction-nav"><li class="' + namespace + 'nav-prev"><a class="' + namespace + 'prev" href="#"><i class="bi bi-arrow-left-circle"></i></a></li><li class="' + namespace + 'nav-next"><a class="' + namespace + 'next" href="#"><i class="bi bi-arrow-right-circle"></i></a></li></ul>');
+          var directionNavScaffold = $('<ul class="' + namespace + 'direction-nav"><li class="' + namespace + 'nav-prev"><a class="' + namespace + 'prev" href="#"></a></li><li class="' + namespace + 'nav-next"><a class="' + namespace + 'next" href="#"></a></li></ul>');
 
           // CUSTOM DIRECTION NAV:
           if (slider.customDirectionNav) {
